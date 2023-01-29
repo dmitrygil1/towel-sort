5e54ba8cd71490566e7b7326f6c6457581d18d32
DmitryGil <91392061+dmitrygil1@users.noreply.github.com>
2023-01-29 13:12:20 +0300

    fix: some bug with return

--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-module.exports = function towelSort (matrix) {if (matrix) {matrix.map((elem, ind) => {if (ind % 2) {elem.reverse()}})return matrix.flat();} else {return [];}}
+module.exports = function towelSort (matrix) {if (matrix) {matrix.map((elem, ind) => {if (ind % 2) {elem.reverse()}}) return matrix.flat();} else { return []; }}
